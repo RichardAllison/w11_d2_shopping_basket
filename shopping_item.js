8d56406c7e72e3aa38ef56c5ff8a8770dcebda86
Richard Allison <richard.allison.18@outlook.com>
2018-02-13 15:27:14 +0000

    Shopping item has price

--- shopping_item.js
+++ shopping_item.js
@@ -1,5 +1,6 @@
-const ShoppingItem = function(name) {
+const ShoppingItem = function(name, price) {
     this.name = name;
+    this.price = price;
 }
 
 module.exports = ShoppingItem;
--- specs/shopping_item_spec.js
+++ specs/shopping_item_spec.js
@@ -6,8 +6,14 @@ describe('Shopping Item', function() {
   it('should have a name', function() {
     const item = new ShoppingItem('Milk');
     const actual = item.name;
-    assert.strictEqual(actual, 'Milk')
+    assert.strictEqual(actual, 'Milk');
   });
+
+  it('should have a price', function() {
+    const item = new ShoppingItem('Milk', 1.50);
+    const actual = item.price;
+    assert.strictEqual(actual, 1.50);
+  })
 
 
 
